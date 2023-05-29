df817e14b0101ced6eb79ae5506923925f427016
Priya Pahwa <pahwa.priya19@gmail.com>
2023-05-29 22:35:39 +0530

    Add view for Google calendar auth
    Signed-off-by: Priya Pahwa <pahwa.priya19@gmail.com>

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-# from api.views import GoogleLogin
-from api.views import HomeView
+from django.urls import path, include
+from api.views import HomeView, GoogleCalendarInitView
 
 urlpatterns = [
-    # path("rest-auth/google/", GoogleLogin.as_view(), name="google_login"),
     path("home/", HomeView.as_view(), name="home"),
+    path('rest/v1/calendar/init/', GoogleCalendarInitView.as_view(), name='google_calendar_init'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,19 @@
-# from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-# from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-# from dj_rest_auth.registration.views import SocialLoginView
-
-# class GoogleLogin(SocialLoginView):
-#     adapter_class = GoogleOAuth2Adapter
-
 from django.views.generic import TemplateView
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class GoogleCalendarInitView(APIView):
+    def get(self, request):
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'client_secret.json', SCOPES, redirect_uri='http://localhost:8000/home/')
+        auth_url, _ = flow.authorization_url(prompt='consent')
+        return Response({'auth_url': auth_url}, status=status.HTTP_200_OK)
